36db659e1da83f83f96875413f97b623dda12a8d
Nuno <nunommarquespt@gmail.com>
2017-03-07 23:18:43 +0000

    Done log out, to test further on.

--- app/src/main/java/com/example/jmfs1/ebec/MainActivity.java
+++ app/src/main/java/com/example/jmfs1/ebec/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jmfs1.ebec;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
@@ -54,6 +57,43 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.log_out) {
+
+            // Remove data in users phone
+            SharedPreferences.Editor prefs = getApplicationContext().getSharedPreferences("LOGIN_PREFS", 0).edit();
+            prefs.clear();
+            prefs.commit();
+
+            // After logout redirect user to Login Activity
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Staring Login Activity
+            getApplicationContext().startActivity(i);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
     @Override
@@ -81,8 +121,8 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
 
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
-    drawer.closeDrawer(GravityCompat.START);
-    return true;
-}
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
