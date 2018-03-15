386cfe7722a9ade71a15d5d942efb79ac088df3c
Diego Caldeira Hernandez <dc.hernandez@ua.pt>
2018-03-15 00:00:24 +0000

    Partnership draft. Dinis complete

--- app/src/main/java/com/example/jmfs1/ebec/Fotos/Fotografias.java
+++ app/src/main/java/com/example/jmfs1/ebec/Fotos/Fotografias.java
@@ -1,5 +1,7 @@
 package com.example.jmfs1.ebec.Fotos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -27,53 +29,85 @@ public class Fotografias extends Fragment {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View myView = inflater.inflate(R.layout.activity_fotografias, container, false);
+        View myView =inflater.inflate(R.layout.activity_fotografias, container, false);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fotografias);
 
-        toolbar = (Toolbar) myView.findViewById(R.id.toolbar);
+       // toolbar = (Toolbar) myView.findViewById(R.id.toolbar);
         //getSupportActionBar().setTitle("Fotografias");
-        viewPager = (ViewPager) myView.findViewById(R.id.viewPager);
-        setupViewPager(viewPager);
+        //viewPager = (ViewPager) myView.findViewById(R.id.viewPager);
+        //setupViewPager(viewPager);
 
-        tabLayout = (TabLayout) myView.findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        //tabLayout = (TabLayout) myView.findViewById(R.id.tabs);
+        //tabLayout.setupWithViewPager(viewPager);
 
         return myView;
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
-        adapter.addFragment(new MinhasFotos(), "Minhas");
-        adapter.addFragment(new OutrasFotos(), "Comunidade");
-        viewPager.setAdapter(adapter);
+    public void gototugaBox(View view) { goToUrl("https://www.tugabox.pt/?lang=en-en");    }
+
+    public void gotoitGrow(View view) { goToUrl("www.itgrow.pt/");    }
+
+    public void gotoshoyce(View view) { goToUrl("shoyce.pt/pt/");    }
+
+    public void gotomirtiflor(View view) { goToUrl("https://pt-pt.facebook.com/Mirtiflor-745004355521875/");    }
+
+    public void gotodiatosta (View view) { goToUrl("http://diatosta.pt/pt/");   }
+
+
+    public void gotoua(View view) {
+        goToUrl("https://www.ua.pt/");
     }
 
-    class ViewPagerAdapter extends FragmentStatePagerAdapter
-    {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+    public void gotoipdj(View view) {
+        goToUrl("http://www.ipdj.pt/");
     }
+
+    public void gotoaauav(View view) {
+        goToUrl("http://www.aauav.pt/");
+    }
+
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+//    private void setupViewPager(ViewPager viewPager) {
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+//        adapter.addFragment(new MinhasFotos(), "Minhas");
+//        adapter.addFragment(new OutrasFotos(), "Comunidade");
+//        viewPager.setAdapter(adapter);
+//    }
+//
+//    class ViewPagerAdapter extends FragmentStatePagerAdapter
+//    {
+//        private final List<Fragment> mFragmentList = new ArrayList<>();
+//        private final List<String> mFragmentTitleList = new ArrayList<>();
+//
+//        public ViewPagerAdapter(FragmentManager manager) {
+//            super(manager);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            return mFragmentList.get(position);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return mFragmentList.size();
+//        }
+//
+//        public void addFragment(Fragment fragment, String title) {
+//            mFragmentList.add(fragment);
+//            mFragmentTitleList.add(title);
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return mFragmentTitleList.get(position);
+//        }
+//    }
 }
